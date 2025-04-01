385b37fdaf4795e30c3d16abc0d36feb05d0f0b3
thinhsuy2908 <phan90539@gmail.com>
2025-04-01 21:18:20 +0700

    Update main.js

--- sesion26/bt9/main.js
+++ sesion26/bt9/main.js
@@ -1,17 +1,25 @@
-let input = prompt("Hãy Nhập email");
-let users= [];
-function isStrongEmail(email) {
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regex.test(email);
+function nhapMang() {
+    let n;
+    do {
+        n = Number(prompt("Nhập số lượng phần tử trong mảng (từ 10 đến 20):"));
+    } while (n < 10 || n > 20 || isNaN(n));
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(Number(prompt(`Nhập phần tử thứ ${i + 1}:`)));
+    }
+    console.log("Mảng vừa nhập:", arr);
+    return arr;
 }
-if (!isStrongEmail(input)) {
-    alert("email ko hợp lệ!");
-    return;
+function tinhTongSoChan(arr) {
+    let tongChan = arr.filter(num => num % 2 === 0).reduce((sum, num) => sum + num, 0);
+    console.log("Tổng các số chẵn:", tongChan);
+    return tongChan;
 }
-if (users.includes(input)) {
-    alert("Tài khoản đã tồn tại!");
-} else {
-    users.push(input);
-    alert("Đăng ký thành công!");
+function tinhTongSoLe(arr) {
+    let tongLe = arr.filter(num => num % 2 !== 0).reduce((sum, num) => sum + num, 0);
+    console.log("Tổng các số lẻ:", tongLe);
+    return tongLe;
 }
-isStrongEmail(input);
+let mangSoNguyen = nhapMang();
+tinhTongSoChan(mangSoNguyen);
+tinhTongSoLe(mangSoNguyen);
